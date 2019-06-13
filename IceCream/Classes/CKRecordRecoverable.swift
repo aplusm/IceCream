5a55fc95b407064f75e63eae5f59b103704764a7
aplusm <aurelien.martinet@gmail.com>
2019-06-13 08:46:09 +0200

    Add finalize to CKRecordRecoverable

--- IceCream/Classes/CKRecordRecoverable.swift
+++ IceCream/Classes/CKRecordRecoverable.swift
@@ -9,7 +9,7 @@ import CloudKit
 import RealmSwift
 
 public protocol CKRecordRecoverable {
-    
+  func finalize(_ realm: Realm)
 }
 
 extension CKRecordRecoverable where Self: Object {
@@ -86,6 +86,7 @@ extension CKRecordRecoverable where Self: Object {
                 o.setValue(recordValue, forKey: prop.name)
             }
         }
+        o.finalize(realm)
         return o
     }
     
